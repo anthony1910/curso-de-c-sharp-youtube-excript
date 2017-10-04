52d2eb674a3e4c78a81c15d7c5952c10e6a6a7e8
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-04 11:44:06 -0300

    Terminando Aula 03, C# Project

---
+++ Projeto/Aula0003/Aula0003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;// Quais os pacotes que nós vamos utilizar(Biblioteca)
+
+namespace Aula0003// Agrupador de instruções
+{
+    class EXcript// Modelo de um objeto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eXcript.");
+        }
+    }
+
+    class Classe2
+    {
+
+    }
+}
