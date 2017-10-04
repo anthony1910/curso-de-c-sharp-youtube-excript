a4cf91f743b29a3775113f67fe5f844dad776e79
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-04 12:45:24 -0300

    Terminando Aula 07, Características das variáveis

---
+++ Projeto/Aula7-CaracterísticasDasVariaveis/Aula7-CaracterísticasDasVariaveis/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace VariaveisIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Características das Variáveis
+            /*
+             * 1) Nome
+             * 2) Tipo
+             * 3) Tamanho
+             * 4) Valor
+             * Colocar na memória apenas o que vamos utilizar
+             * 
+             * 1) Armazenamento é temporário
+             * 2) Os dados sempre serão removidos da memória
+             * 3) Os dados contidos nas variávies são voláteis
+             * 4) O valor contido em uma variável é sobrescrito pelo novo valor
+             * 5) O C# desaloca a memória automaticamente
+            */
+
+            int numeroTotal;// Variáveis não podem conter espaços. Primeiro nome minúsculo e o segundo é maiúsculo, por convensão. Não pode começar com número. Colocar _ se preciso
+        }
+    }
+}
