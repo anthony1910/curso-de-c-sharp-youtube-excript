0318c34ce6e355bf7906919ac53dc0dd3ae4beb3
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-04 11:34:58 -0300

    Terminando Aula 02, Console Application

---
+++ Projeto/Aula0002/Aula0002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula0002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A mensagem."); // Console = Classe para Prompt de Comando. Possui muitos métodos
+        }
+    }
+}
---
+++ Projeto/Introdução/Introdução/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Introdução
+{
+    class Program
+    {
+        static void Main(string[] args) // Primeira função a ser procurada
+        {
+            Console.WriteLine("Hello World. Sejam todos bem-vindos.");            
+        }
+    }
+}
