21b3b98f16174d01bb9b566b8f7f336c5c29d2da
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-10-04 11:59:53 -0300

    Terminando Aula 5, Variáveis I

---
+++ Projeto/Aula0005 - Variáveis/Aula0005 - Variáveis/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variável é um local na memória que reservamos para armazenar um valor
+            int num = 200;// Não é possível alterar o tipo da variável durante o código
+            Console.WriteLine(num);
+
+            bool b = true;
+
+            string s = "Aqui eu posso escrever textos";
+
+            Console.WriteLine(b);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ Projeto/Aula0005 - Variáveis/Aula0005 - Variáveis/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Aula0005 - Variáveis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Aula0005 - Variáveis")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a92b608f-e7ff-4bf3-a888-fe772be9a9d6")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// utilizando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
